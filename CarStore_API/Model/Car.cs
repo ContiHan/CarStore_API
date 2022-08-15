95e12c34e4da47be9329058753e173d82c1754a6
contihan <daniel.hanak@continental-corporation.com>
2022-08-15 11:08:10 +0200

    car model

---
+++ CarStore_API/Model/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace CarStore_API.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Condition { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string BodyType { get; set; }
+        public string Fuel { get; set; }
+        public string WheelDrive { get; set; }
+        public string UsageType { get; set; }
+        public double Price { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
